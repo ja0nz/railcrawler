008f75d83f0e58f82086ce5fe39975a2f7fec2f0
Janz <mail@ja.nz>
2017-07-04 16:34:18 +0200

    code branching on empty values

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const execFile = require("child_process").execFile;
 const CHROMIUM = "/usr/bin/chromium";
 const cheerio = require("cheerio");
+const Result = require("folktale/result");
 import { bahnQs } from "./queryString";
 import { futurize } from "futurize";
 import { appendFile } from "fs";
@@ -11,12 +12,13 @@ import {
   replace,
   join,
   map,
+  flatten,
   compose,
   split,
   reverse,
-  isEmpty,
+  invoker,
+  identity,
   filter,
-  not,
   prepend
 } from "ramda";
 
@@ -33,25 +35,36 @@ const composeQs = compose(
   compose(getDateString, getDate, getDay)
 );
 
-const main = day => {
-  fetch(CHROMIUM, ["--headless", "--disable-gpu", "--dump-dom", composeQs(day)])
-    .map(cheerio.load)
-    .map(dom => dom(".fareOutput").text().toString())
-    .map(split(/\sEUR/g))
-    .map(filter(compose(not, isEmpty)))
-    .map(map(replace(",", ".")))
-    .map(map(Number))
-    .map(prepend(new Date(queryDate(day)).toISOString()))
-    .map(prepend(new Date().toISOString()))
-    .map(join(","))
-    .chain(s => appendToFile("railPrices.csv", s + "\n"))
-    .fork(console.error, _ => console.log("success"));
+const parse = dom => {
+  const $ = cheerio.load(dom);
+  return $(".firstrow").map((_, e) => {
+    const isEmpty = v => (v ? v : null);
+    const seq = map(x =>
+      Result.fromNullable(isEmpty($(".fareOutput", x, e).text()))
+    );
+
+    const [red, std] = seq([".farePep", ".fareStd"]);
+    return std.map(b => [red.getOrElse(b), b]).fold(console.error, v => v);
+  });
 };
 
 const randomMs = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const main = day => {
+  fetch(CHROMIUM, ["--headless", "--disable-gpu", "--dump-dom", composeQs(day)])
+    .map(compose(invoker(0, "get"), parse))
+    .map(map(compose(filter(identity), split(/\sEUR/g))))
+    .map(flatten)
+    .map(map(compose(Number, replace(",", "."))))
+    .map(prepend(new Date(queryDate(day)).toISOString()))
+    .map(prepend(new Date().toISOString()))
+    .map(join(","))
+    .chain(s => appendToFile("railPrices.csv", s + "\n"))
+    .fork(console.error, i => console.log(i));
+};
 
 (async function run(day) {
   main(day);
